b0a6fdaca0331c5b3ff97f96c57eb8fb81555f87
Evgenii Rubin <kubikus2009@yandex.ru>
2026-03-04 13:46:54 +0100

    chore(tailwind): fix styles content glob warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/styles/**/*.{css}"
+    "./src/styles/**/*.css"
   ],
   theme: {
     container: {
